24d3e9906eceb43dbd168e989b610e86ce06cf27
Avinashkurumella <141439503+Avinashkurumella@users.noreply.github.com>
2023-11-30 22:18:53 +0530

    Delete Rent-main directory

--- Rent-main/frontend/src/app/property-owner.model.ts
+++
@@ -1,8 +0,0 @@
-export interface PropertyOwner {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  contactRequestedBy: string | null;
-
-}
--- Rent-main/frontend/src/app/tenant.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Tenant {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-}
